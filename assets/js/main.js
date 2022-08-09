5664df9501c828de373b598234e1a7bc181074e6
devcarolinealmeida <carolineitalia85@gmail.com>
2022-08-09 12:39:36 +0200

    completed clock

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,18 +3,26 @@ let clock = () => {
     let hours = date.getHours();
     let mins = date.getMinutes();
     let secs = date.getSeconds();
-    let time = `${hours}:${mins}:${secs}`;
 
     let period = "AM";
-    if (hours == 0) hours = 12;
-
-    if (hours > 12) {
-    hours = hours - 12;
-    period = "PM";   
-}
-console.log(time, period)
+    if (hours == 0)  {
+        hours = 12;
+    } else if (hours >= 12) {
+        if (hours > 12) {
+            hours = hours - 12;
+            period = "PM"; 
+        }
+        hours = hours < 10 ? `0${hours}` : hours;
+        mins = mins < 10 ? `0${mins}` : mins;
+        secs = secs < 10 ? `0${secs}` : secs;
+    }
+    let time = `${hours}:${mins}:${secs} ${period}`;
+    document.getElementById("clock").innerHTML = time
+    setTimeout(clock, 1000)
 }
 
 clock()
+
+
 
 
